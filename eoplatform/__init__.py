f6a439207f059b2b326c2a66f197b9424cf6fa29
mtralka <mtralka@umd.edu>
2021-10-29 16:44:34 -0400

    :bookmark: v0.2.2 change

--- eoplatform/__init__.py
+++ eoplatform/__init__.py
@@ -1,6 +1,5 @@
 from eoplatform.main import info
 from eoplatform.platforms.download.downloadControl import download
-from eoplatform.status import status
 
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
