6de51597ccb766321740dbb829489375ae64d822
mtralka <mtralka@umd.edu>
2021-11-10 23:08:27 -0500

    :sparkles: Convert metadata func to Pathlib

--- eoplatform/metadata/metadata.py
+++ eoplatform/metadata/metadata.py
@@ -22,7 +22,7 @@ def _not_found_message(*args: str) -> None:
 
 
 def extract_metadata(
-    file_path: Union[Path, str], target_attributes: List[str], **kwargs: Any
+    file_path: Union[Path, str], target_attributes: Union[List[str], str], **kwargs: Any
 ) -> Union[Dict[str, Optional[str]], Dict[str, str]]:
     """Extract metadata from file
 
@@ -33,7 +33,7 @@ def extract_metadata(
     ----------
     file_path : str
         Full file path to target XML file
-    target_attributes: List[str]
+    target_attributes: Union[List[str], str]
         List of target attributes desired
 
     Returns
@@ -51,15 +51,15 @@ def extract_metadata(
     file_extension = file_extension.lower()
 
     if file_extension == ".xml":
-        return extract_XML_metadata(str(file), target_attributes)
+        return extract_XML_metadata(file, target_attributes)
     elif file_extension == ".txt":
-        return extract_TXT_metadata(str(file), target_attributes, **kwargs)
+        return extract_TXT_metadata(file, target_attributes, **kwargs)
     else:
         raise ValueError(f"{file_extension} not currently supported")
 
 
 def extract_XML_metadata(
-    file_path: str, target_attributes: List[str]
+    file_path: Union[str, Path], target_attributes: Union[List[str], str]
 ) -> Dict[str, str]:
     """Extract metadata from XML file
 
@@ -69,9 +69,9 @@ def extract_XML_metadata(
 
     Parameters
     ----------
-    file_path : str
+    file_path : Union[Path, str]
         Full file path to target XML file
-    target_attributes: List[str]
+    target_attributes: Union[List[str], str]
         List of target attributes desired
 
     Returns
@@ -80,13 +80,21 @@ def extract_XML_metadata(
 
     """
 
-    X_PATH_WILDCARD: str = ".//"
+    X_PATH_WILDCARD: Final[str] = ".//"
 
-    if not path.exists(file_path):
-        raise FileNotFoundError(f"{file_path} does not exist")
+    file: Path = Path(file_path) if isinstance(file_path, str) else file_path
+    target_attributes_list: List[str] = (
+        [target_attributes] if isinstance(target_attributes, str) else target_attributes
+    )
 
-    _, file_extension = path.splitext(file_path)
-    if file_extension != ".xml":
+    if not file.exists():
+        raise FileNotFoundError(f"{file} does not exist")
+
+    file_extension = file.suffix
+
+    if not file_extension:
+        raise ValueError("File must have a `.xml` file extension")
+    if file_extension.lower() != ".xml":
         raise TypeError(f"{file_path} is not an XML file")
 
     namespaces: Dict[str, str] = dict(
@@ -96,7 +104,7 @@ def extract_XML_metadata(
     tree: ET.ElementTree = ET.parse(file_path)
     found_attributes: Dict[str, str] = {}
 
-    for target_attribute in target_attributes:
+    for target_attribute in target_attributes_list:
 
         target_el: Optional[ET.Element] = tree.find(
             X_PATH_WILDCARD + target_attribute, namespaces=namespaces
@@ -112,7 +120,9 @@ def extract_XML_metadata(
 
 
 def extract_TXT_metadata(
-    file_path: str, target_attributes: List[str], delineator: str = "="
+    file_path: Union[Path, str],
+    target_attributes: Union[List[str], str],
+    delineator: str = "=",
 ) -> Dict[str, Optional[str]]:
     """Extract metadata from TXT file
 
@@ -123,9 +133,9 @@ def extract_TXT_metadata(
 
     Parameters
     ----------
-    file_path : str
+    file_path : Union[Path, str]
         Full file path to target TXT file
-    target_attributes: List[str]
+    target_attributes: Union[List[str], str]
         List of target attributes desired
 
     Returns
@@ -134,19 +144,29 @@ def extract_TXT_metadata(
 
     """
 
-    if not path.exists(file_path):
+    file: Path = Path(file_path) if isinstance(file_path, str) else file_path
+    target_attributes_list: List[str] = (
+        [target_attributes] if isinstance(target_attributes, str) else target_attributes
+    )
+
+    if not file.exists():
         raise FileNotFoundError(f"{file_path} does not exist")
 
-    _, file_extension = path.splitext(file_path)
-    if file_extension != ".txt":
+    file_extension = file.suffix
+
+    if not file_extension:
+        raise ValueError("File must have a `.txt` file extension")
+    if file_extension.lower() != ".txt":
         raise TypeError(f"{file_path} is not a TXT file")
 
-    found_attributes: Dict[str, Optional[str]] = {k: None for k in target_attributes}
+    found_attributes: Dict[str, Optional[str]] = {
+        k: None for k in target_attributes_list
+    }
 
-    with open(file_path) as file:
-        for line_number, line in enumerate(file):
+    with file.open() as f:
+        for line_number, line in enumerate(f):
 
-            split: List[str] = line.split("=")
+            split: List[str] = line.split(delineator)
             split = [x.strip(" ") for x in split]
 
             if not len(split) <= 2:
@@ -154,7 +174,7 @@ def extract_TXT_metadata(
                     f"Line {line_number} violates formatting assumptions"
                 )
 
-            if split[0] not in target_attributes:
+            if split[0] not in target_attributes_list:
                 continue
 
             if len(split) != 2:
@@ -166,7 +186,7 @@ def extract_TXT_metadata(
 
     if not all(found_attributes.values()):
         not_found: List[str] = list(
-            set(target_attributes)
+            set(target_attributes_list)
             - set(k for k, v in found_attributes.items() if v is not None)
         )
         _not_found_message(*not_found)
